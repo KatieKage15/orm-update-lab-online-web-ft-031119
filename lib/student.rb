b5a81c31a027d88c27f5b156f751e0b7a8de89a8
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-15 09:39:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,18 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def
+  def save
+    sql = <<-SQL
+      if self.id
+        self.update
+      else 
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
 
 
 end
